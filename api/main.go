f49ca760d7d33b9aed4d37b2565d8de93d19bfc6
V <contact@vedrans.me>
2023-11-07 08:56:25 +0000

    data struct, init and step functions

--- api/main.go
+++ api/main.go
@@ -5,6 +5,27 @@ import (
 	"net/http"
 )
 
+// data structure to hold the fibonacci numbers
+type findmynacci struct {
+	current uint64
+	previous uint64
+}
+
+// function to initialize the findmynacci struct
+func newFindmynacci() *findmynacci {
+	return &findmynacci{current: 1, previous: 0}
+}
+
+// steps forward with the fibonacci sequence
+func (f *findmynacci) fwd() {
+	f.current, f.previous = f.current + f.previous, f.current
+}
+
+// steps backward with the fibonacci sequence
+func (f *findmynacci) bwd() {
+	f.current, f.previous = f.previous, f.current - f.previous
+}
+
 func main() {
 	
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
